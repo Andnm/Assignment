e6f5aee7350f8193c3cf4d31c1cc65ca1eb05da3
Andnm <dangnguyenminhan123@gmail.com>
2022-07-19 06:56:02 +0700

    Single Page Applications Part 1

--- src/component/MenuComponent.js
+++ src/component/MenuComponent.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-
-
 function RenderMenuItem({ dish }) {
     return (
         <Card>
